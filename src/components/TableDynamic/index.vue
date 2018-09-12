c12f897c41c5ea0b9e49358b466c0042d19ffa36
qinxuehan <729511058@qq.com>
2018-09-12 18:20:43 +0800

    page

--- src/components/TableDynamic/index.vue
+++ src/components/TableDynamic/index.vue
@@ -14,7 +14,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for='(item,index) in formArray' v-bind:key="index">
+          <tr v-for='(item,index) in pageData' v-bind:key="index">
             <td><span>{{index+1}}</span></td>
               <td v-for="(node,index) in Object.keys(theadList)" v-bind:key="index">
                   <input type="text" v-model="item[node]" class="input-box"/>
@@ -23,6 +23,21 @@
           </tr>
         </tbody>
       </table>
+    </div>
+    <div class="page-turn">
+      <div class="record">共
+        <span>{{pageParams.total}}</span>条记录 第 {{pageParams.currentPage}}/{{Math.ceil(pageParams.total/pageParams.pageSize)}}页
+      </div>
+      <div class="page-wrap">
+        <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        background :current-page="pageParams.currentPage"
+        :page-size="pageParams.pageSize"
+        layout="prev, pager, next, jumper"
+        :total="pageParams.total">
+        </el-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -36,20 +51,51 @@ export default {
       // isempty: '',
       theadList: {name: '姓名', sex: '性别', age: '年龄', address: '住址', hobby: '爱好'},
 
-      formArray: []
+      formArray: [],
+      // 总记录条数
+      totalData: [],
+      // 分页所需记录条数
+      pageData: [],
+      // 分页参数
+      pageParams: {
+        currentPage: 1,
+        total: 1,
+        pageSize: 5
+      }
     }
   },
   // props: {
   //   theadList: Object
   // },
   methods: {
+    // 分页大小改动
+    handleSizeChange (val) {
+      this.pageParams.pageSize = val
+      this.initPageData()
+    },
+    // 分布当前页数改动
+    handleCurrentChange (val) {
+      this.pageParams.currentPage = val
+      this.initPageData()
+    },
+    // 初始化分页数据
+    initPageData () {
+      const start =
+        (this.pageParams.currentPage - 1) * this.pageParams.pageSize
+      this.pageData = this.formArray.slice(
+        start,
+        start + this.pageParams.pageSize
+      )
+    },
     // 向末尾插入一行
     handleInsert () {
       this.formIncrement()
+      this.initPageData()
     },
     // 删除指定的一行
     handleDelSingle (index) {
       this.formArray.splice(index, 1)
+      this.initPageData()
     },
     // 表单数组自增
     formIncrement () {
@@ -64,14 +110,26 @@ export default {
     // 初始化表格的行数
     for (let i = 0; i < 5; i++) {
       this.formIncrement()
-    }
+    };
+    this.pageParams.total = 1
+    this.initPageData()
   },
   watch: {
     formArray: {
       handler (val) {
-        console.log('val', val)
+        this.pageParams.total = this.formArray.length
         // 打印出表单变化后的值
         this.$emit('input', val)
+      },
+      deep: true
+    },
+    // 解决删除条数时自动翻页
+    pageData: {
+      handler: function (val) {
+        if (!this.pageData.length) {
+          --this.pageParams.currentPage
+          this.initPageData()
+        }
       },
       deep: true
     }
